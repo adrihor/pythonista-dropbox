4324362b5279e37fb43435ca781dfe5e5d1e118e
dmmmd <dmmmdfll@gmail.com>
2015-11-29 11:02:56 +0000

    improved doc string in adapters

--- pythonista_dropbox/adapters.py
+++ pythonista_dropbox/adapters.py
@@ -3,22 +3,37 @@ import importlib
 
 
 class Platform(object):
+    """The Platform class is used to test if the Pythonista app is the
+    interpreter based on 'Darwin-15.0.0.' presence in platform.platform()
+    return value."""
     platform = platform.platform()
 
-    """Use to distinguish Pythonista interpreter from non-Pythonista
-    interpreter."""
-
     def __init__(self):
-        """set pythonista to True if on iOS device
-        assume that platform.platform returns a string with Darwin-15.0.0 in it"""
+        """Sets self.pythonista to True if on iOS device."""
         self.pythonista = True if "Darwin-15.0.0" in self.platform else False
 
 
 class PythonistaModuleAdapter(object):
-    platform = Platform()
+    """Class PythonistaModuleAdapter substitutes for Pythonisa-only
+    modules to aid in Pythonista development on non-iOS platforms.
+    Example usage for clipboard:
+    DO NOT import the module clipboard.
+    Instead, do this:
+        clipboard = PythonistaModuleAdapter('clipboard')
+    Now when you use clipboard on a non-Pythonista platform, any callable
+    attribute of the equally named Pythonista module will accept all
+    args and kwargs and return None.
+    If the script is used in Pythonista, clipboard behaves exactly as if it
+    were imported.
+    To use in the case where the modules have differing names, do this:
+        try:
+            import keyring as keychain  # non-Pythonista
+        except ImportError:
+            import keychain # Pythonista
 
-    """substitute for Pythonisa-only modules to aid in Pythonista development
-    on non-iOS platform, instantiate and then can mock any function on the module"""
+        keychain = PythonistaModuleAdapter('keychain')
+        keychain.keychain = keyring"""
+    platform = Platform()
 
     def __init__(self, module):
         if self.platform.pythonista:
@@ -32,8 +47,9 @@ class PythonistaModuleAdapter(object):
         return
 
     def __getattr__(self, attr):
-        """Returns attr of module 'module' if has attr or mock_function if not.
-        On non-Pythonista platform mock_function is returned. On Pythonista platform,
+        """Returns attr of module if has attr or mock_function if not.
+        On non-Pythonista platform mock_function is returned.
+        On Pythonista platform,
         the attr of the module is returned."""
         try:
             attrs = getattr(getattr(self, self.module), attr)
