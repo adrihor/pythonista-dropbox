c5ced3d4f7c5552c797eaff796e1ced6d68a3ee1
dmmmd <dmmmd@gmail.com>
2015-11-29 20:10:34 -0500

    checking for access set on keyring

--- pythonista_dropbox/client.py
+++ pythonista_dropbox/client.py
@@ -8,8 +8,10 @@ keychain_key_words = (
     ('dmmmd sync', 'access token key', ),
     ('dmmmd sync', 'access token secret', ),
 )
-ACCESS_KEY, ACCESS_SECRET = [keychain.get_password(service, account)
-                             for service, account in keychain_key_words]
+ACCESS = ACCESS_KEY, ACCESS_SECRET = [keychain.get_password(service, account)
+                                      for service, account
+                                      in keychain_key_words]
+assert all(ACCESS), "Run request-auth-token to set access key and secret."
 
 
 def get_client(access_key=None, access_secret=None):
@@ -17,7 +19,6 @@ def get_client(access_key=None, access_secret=None):
     """
     access_key = ACCESS_KEY if access_key is None else access_key
     access_secret = ACCESS_SECRET if access_secret is None else access_secret
-
     session = get_session()
     session.set_token(access_key, access_secret)
     client = dropbox.client.DropboxClient(session)
