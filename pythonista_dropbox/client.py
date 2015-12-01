72ba5b9b50ecb4347d48e01bcddcb3afcf06ffae
dmmmd <dmmmdfll@gmail.com>
2015-12-01 16:06:33 +0000

    added doc strings to client

--- pythonista_dropbox/client.py
+++ pythonista_dropbox/client.py
@@ -12,11 +12,18 @@ keychain_key_words = (
 ACCESS = ACCESS_KEY, ACCESS_SECRET = [keychain.get_password(service, account)
                                       for service, account
                                       in keychain_key_words]
-assert all(ACCESS), "Run request-auth-token to set access key and secret."
+assert all(ACCESS), \
+    ' '.join((
+        "Run request-auth-token at the"
+        "command line to set access key and secret.",
+        "Refer to https://www.dropbox.com/developers/reference/oauth-guide"
+        "for detailed guidance."
+    ))
 
 
 def get_client(access_key=None, access_secret=None):
     """returns a dropbox client
+    see https://www.dropbox.com/developers-v1/core/start/python
     """
     access_key = ACCESS_KEY if access_key is None else access_key
     access_secret = ACCESS_SECRET if access_secret is None else access_secret
