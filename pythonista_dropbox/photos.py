fec428fcb5826afb800654eb3e5615aa6f57cb10
dmmmd <dmmmdfll@gmail.com>
2015-12-02 11:54:34 +0000

    passing photo mock test

---
+++ pythonista_dropbox/photos.py
@@ -0,0 +1,80 @@
+import sys
+import os
+from pythonista_dropbox.adapters import PythonistaModuleAdapter
+
+photos = PythonistaModuleAdapter('photos')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+mock_photo = os.path.join(BASE_DIR, 'mock_photo')
+photo_meta_data = os.path.join(mock_photo, 'exifs')
+images_dir = os.path.join(mock_photo, 'images')
+
+
+kwargs = dict(
+    show_albums=True,
+    include_metadata=True,
+    original=False,
+    raw_data=True,
+    multi=True
+)
+
+
+def pick_image(
+    *args,
+    **kwargs
+
+):
+    """Docstring for pick_image.
+    Mock out the Pythonista version of photos.pick_image
+    Return what picker in iOS would return
+    """
+    import json
+    from StringIO import StringIO
+
+    exifs = []
+    _, _, files = os.walk(photo_meta_data).next()
+    for file in files:
+        with open(os.path.join(photo_meta_data, file), 'r') as fh:
+            exifs.append(json.load(fh))
+    images = []
+    _, _, files = os.walk(images_dir).next()
+    for file in files:
+        with open(os.path.join(images_dir, file), 'r') as fh:
+            binary_image = StringIO(fh.read())
+            binary_image.seek(0)
+            images.append(binary_image)
+    if kwargs['raw_data']:
+        return ((binary_image, photo_meta_data)
+                for binary_image, photo_meta_data
+                in zip(images, exifs))
+    else:
+        return ((type('JPEGImage', (), {})(), photo_meta_data)
+                for photo_meta_data in exifs)
+
+if not photos.pythonista:
+    photos.pick_image = pick_image
+
+
+def get_image_names():
+    """A function that returns a tuple of filenames and their extensions as lists
+    """
+    kwargs['raw_data'] = False
+    selected_photos = photos.pick_image(**kwargs)
+    filenames = [meta_data['filename'].split('.')
+                 for _, meta_data in selected_photos]
+    names, extensions = zip(*filenames)
+    return names, extensions
+
+
+def main():
+    import json
+    import urllib
+    webbrowser = PythonistaModuleAdapter('webbrowser')
+    result = get_image_names()
+    result = json.dumps(result)
+    url = 'drafts4://x-callback/create?text={}'.format(
+        urllib.quote(result))
+    webbrowser.open(url)
+    return 0
+
+    if __name__ == "__main__":
+        sys.exit(main())
--- tests/test_photo.py
+++ tests/test_photo.py
@@ -11,20 +11,21 @@ kwargs = dict(
 extension_names = (
     'JPG',
     'GIF',
+    'PNG',
 )
+extension_names = [name.lower() for name in extension_names]
 
 
 def test_photos():
-    """TODO: Docstring for test_photos.
-    :returns: TODO
-
-    """
+    """test that the PythonistaModuleAdapter mocking out Pythonista's
+    photos.pick_image behaves in an expected way on non-Pythonista platform."""
     if not photos.pythonista:
         assert photos.pick_image
-    selected_photos = photos.pick_image(**kwargs)
-    for binary_image, meta_info in selected_photos:
-        assert binary_image
-        assert 'JPG' in meta_info.get('filename')
+        selected_photos = photos.pick_image(**kwargs)
+        for binary_image, meta_info in selected_photos:
+            assert binary_image
+            assert any([ext in meta_info.get('filename').lower()
+                       for ext in extension_names])
 
 
 def test_get_image_names():
@@ -32,5 +33,7 @@ def test_get_image_names():
 
     """
     names, extensions = photo_module.get_image_names()
-    assert all('IMG' in name for name in names)
-    assert all(extension in extension_names for extension in extensions)
+    assert any(['img' in name.lower()
+                for name in names])
+    assert all([extension.lower() in extension_names
+                for extension in extensions])
